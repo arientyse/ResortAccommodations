872503bb6d01606f7e0b3d270d45df643f4fd72f
Ariana Tyson <arianatyson@live.com>
2019-10-29 05:55:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ResortAccommodations
+ Project 3 for CSC 1301. Focusing on the use of loops in Java.
---
+++ src/edu/gsu/csc1301/resortaccommodations/ResortAccommodations.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.gsu.csc1301.resortaccommodations;
+
+/**
+ * @author Ari
+ *
+ */
+public class ResortAccommodations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
